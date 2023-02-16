8f3956d0e8df18be434a3ac6b4ede0ae8a1329fa
egongs <nikolaivanov959@gmail.com>
2023-02-16 12:12:38 -0500

    animation

--- runner.js
+++ runner.js
@@ -3,7 +3,8 @@ let Runner = function (posX, posY) {
     this.posY = posY;
 }
 
-Runner.prototype.dessiner = function () {
+Runner.prototype.dessinerGaucheFull = function () {
+    objC2D.save()
     objC2D.beginPath()
     objC2D.fillStyle = "white"
     objC2D.fillRect(0, 0 / 2, 12, 10)
@@ -13,8 +14,10 @@ Runner.prototype.dessiner = function () {
     objC2D.rotate(-80 * Math.PI / 180)
     objC2D.fillRect(-9, 20, 4, 12)
     objC2D.closePath()
+    objC2D.restore()
 }
-Runner.prototype.dessiner2 = function () {
+Runner.prototype.dessinerDroiteFull = function () {
+    objC2D.save()
     objC2D.beginPath()
     objC2D.fillStyle = "white"
     objC2D.fillRect(3, 0 / 2, 12, 10)
@@ -24,15 +27,61 @@ Runner.prototype.dessiner2 = function () {
     objC2D.rotate(-80 * Math.PI / 180)
     objC2D.fillRect(-9, 20, 4, 12)
     objC2D.closePath()
+    objC2D.restore()
 }
-Runner.prototype.dessiner3 = function () {
+Runner.prototype.dessinerSpawn = function () {
+    objC2D.save()
     objC2D.beginPath()
     objC2D.fillStyle = "white"
     objC2D.fillRect(1, 0 / 2, 12, 10)
     objC2D.fillRect(4, 10, 7, 12)
-    objC2D.rotate(40 * Math.PI / 180)
-    objC2D.fillRect(15, 10, 4, 12)
-    objC2D.rotate(-80 * Math.PI / 180)
-    objC2D.fillRect(-9, 20, 4, 12)
+    objC2D.fillRect(5.5, 17, 4, 12)
     objC2D.closePath()
+    objC2D.restore()
+}
+Runner.prototype.dessinerGaucheMid = function () {
+    objC2D.save()
+    objC2D.beginPath()
+    objC2D.fillStyle = "white"
+    objC2D.fillRect(0, 0 / 2, 12, 10)
+    objC2D.fillRect(4, 10, 7, 12)
+    objC2D.rotate(15 * Math.PI / 180)
+    objC2D.fillRect(8, 15, 4, 12)
+    objC2D.rotate(-30 * Math.PI / 180)
+    objC2D.fillRect(2, 19, 4, 12)
+    objC2D.closePath()
+    objC2D.restore()
+}
+Runner.prototype.dessinerDroiteMid = function () {
+    objC2D.save()
+    objC2D.beginPath()
+    objC2D.fillStyle = "white"
+    objC2D.fillRect(3, 0 / 2, 12, 10)
+    objC2D.fillRect(4, 10, 7, 12)
+    objC2D.rotate(15 * Math.PI / 180)
+    objC2D.fillRect(8, 15, 4, 12)
+    objC2D.rotate(-30 * Math.PI / 180)
+    objC2D.fillRect(2, 19, 4, 12)
+    objC2D.closePath()
+    objC2D.restore()
+}
+Runner.prototype.dessinerGauche = function () {
+    objC2D.save()
+    objC2D.beginPath()
+    objC2D.fillStyle = "white"
+    objC2D.fillRect(0, 0 / 2, 12, 10)
+    objC2D.fillRect(4, 10, 7, 12)
+    objC2D.fillRect(6, 17, 4, 12)
+    objC2D.closePath()
+    objC2D.restore()
+}
+Runner.prototype.dessinerDroite = function () {
+    objC2D.save()
+    objC2D.beginPath()
+    objC2D.fillStyle = "white"
+    objC2D.fillRect(3, 0 / 2, 12, 10)
+    objC2D.fillRect(4, 10, 7, 12)
+    objC2D.fillRect(6, 17, 4, 12)
+    objC2D.closePath()
+    objC2D.restore()
 }
