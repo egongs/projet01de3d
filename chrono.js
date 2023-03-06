eb49e887ca25476bf87292a5697421e0c9bd3e7d
Just1813 <joost.perron@gmail.com>
2023-03-06 16:43:30 -0500

    fixed winning level

--- chrono.js
+++ chrono.js
@@ -1,23 +1,22 @@
-let Chrono = function(){
+let Chrono = function () {
 }
-Chrono.prototype.chrono = function()
-{
+Chrono.prototype.chrono = function () {
     let seconds = 0;
     let minutes = 0;
 
     function displayTimer() {
-    seconds++;
-    if (seconds == 60) {
-        seconds = 0;
-        minutes++;
-    }
-    if (minutes == 60) {
-        minutes = 0;
-    }
-    
-    let displaySeconds = seconds < 10 ? "0" + seconds : seconds;
-    let displayMinutes = minutes < 10 ? "0" + minutes : minutes;
-    temps = displayMinutes + ":" + displaySeconds
+        seconds++;
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+        }
+        if (minutes == 60) {
+            minutes = 0;
+        }
+
+        let displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+        let displayMinutes = minutes < 10 ? "0" + minutes : minutes;
+        temps = displayMinutes + ":" + displaySeconds
     }
 
     setInterval(displayTimer, 1000);
